0c7b7aaf24b0080fc0b27ea58e4102fc5537378d
Adam Basiak <basiakus@gmail.com>
2018-01-26 23:03:56 +0100

    mLab Database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://<Basiakus>:<Mojehaslo87>@ds217138.mlab.com:17138/database-basiakus', {
     //useMongoClient: true
 });
 
